e3da86d746d9ef5619dbd2d1065f2eb1f1df81ef
Vasileios Georgopoulos <billygeo0@gmail.com>
2020-12-28 10:58:27 +0200

    initial commit

---
+++ demo/index.js
@@ -0,0 +1,42 @@
+import MotorCortex from '@kissmybutton/motorcortex';
+import MotorCortexGraph from "../dist/bundle.umd";
+const MCGraphs = MotorCortex.loadPlugin(MotorCortexGraph);
+
+import Player from "@kissmybutton/motorcortex-player";
+
+
+const clip = new MotorCortex.HTMLClip({
+    html: `<div class="container">
+        <div id="effect"></div>
+        <div id="htmlclip"></div>
+        <div id="combo"></div>
+        <div id="myclip"></div>
+    </div>`,
+    css: `
+        .container{
+            width: 600px,
+            height: 400px
+        }
+        .container>div{
+            width: 50%;
+            height: 50%;
+        }
+    `,
+    host: document.getElementById('clip'),
+    containerParams: {
+        width: '600px',
+        height: '400px'
+    }
+});
+
+
+
+const newGraph = new MCGraphs.ProgressBar({
+    // here goes your attrs
+}, {
+    selector: '#htmlclip'
+});
+
+clip.addIncident(newGraph, 0);
+
+const player = new Player({clip});
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import BarChartSimple from './Incidents/barChartSimple/barChartSimple';
+import ProgressBar from './Incidents/progressBar/progressBar';
+
+export default {
+  npm_name: "motorcortex-graphs", // !! make sure the name of your plugin is identical to the name of your package.json !!
+  incidents: [
+    {
+      exportable: ProgressBar,
+      name: "ProgressBar",
+      // define your attributeValidationRules so MotorCortex can automatically validate them on instantiation 
+      // also so your Incidents are directly embedable to DonkeyClip
+    //   attributesValidationRules: {},
+      originalDims: {
+        width: "600px",
+        height: "400px"
+      }
+    },
+    {
+      exportable: BarChartSimple,
+      name: "BarChartSimple",
+      // define your attributeValidationRules so MotorCortex can automatically validate them on instantiation 
+      // also so your Incidents are directly embedable to DonkeyClip
+    //   attributesValidationRules: {},
+      originalDims: {
+        width: "600px",
+        height: "400px"
+      }
+    },
+  ]
+};
